cd433964fa139c6ce81bfe8b9e83cbee3864f7a8
alextope <alex56480@gmail.com>
2019-09-24 01:12:28 +0000

    Customer and Product entity created with make:entity

---
+++ src/Entity/Test.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TestRepository")
+ */
+class Test
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $test;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $no;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTest(): ?string
+    {
+        return $this->test;
+    }
+
+    public function setTest(?string $test): self
+    {
+        $this->test = $test;
+
+        return $this;
+    }
+
+    public function getNo(): ?string
+    {
+        return $this->no;
+    }
+
+    public function setNo(string $no): self
+    {
+        $this->no = $no;
+
+        return $this;
+    }
+}
